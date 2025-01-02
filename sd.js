93e3cb275c8c4b26323242ee9727473ff8d1d4b4
GHVIP <106148667+GHVIP@users.noreply.github.com>
2025-01-02 21:33:43 +0800

    Update sd.js

--- sd.js
+++ sd.js
@@ -34,27 +34,27 @@ async function getClassList(args) {
     backData.data = [
         {
             type_id: '1',
-            type_name: '电影',
+            type_name: '闪电电影',
             hasSubclass: false,
         },
         {
             type_id: '2',
-            type_name: '剧集',
+            type_name: '闪电剧集',
             hasSubclass: false,
         },
         {
             type_id: '3',
-            type_name: '动漫',
+            type_name: '闪电综艺',
             hasSubclass: false,
         },
         {
             type_id: '4',
-            type_name: '纪录片',
+            type_name: '闪电动漫',
             hasSubclass: false,
         },
         {
-            type_id: '25',
-            type_name: '综艺片',
+            type_id: '30',
+            type_name: '闪电短剧',
             hasSubclass: false,
         },
     ]
